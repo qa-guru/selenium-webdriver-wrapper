a7ccec21ac5bf2b7a3fc36eb2e994a0bd23b6d65
yashaka <yashaka@gmail.com>
2022-09-23 21:28:57 +0300

    encapsulate all high level browser commands into object of Browser class
    i.e. apply OOP paradigm
    to be able to e.g. use two browsers in one test in a more straightforward and natural way
    (otherwise you would need to hack global driver field of a browser module to switch between browsers)

--- seleyasha/browser.py
+++ seleyasha/browser.py
@@ -1,52 +1,59 @@
+from selenium.common import WebDriverException
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.remote.webelement import WebElement
 
 from seleyasha.selector import to_locator
 from seleyasha.wait import WebDriverWait
 
-driver: WebDriver = ...
-wait: WebDriverWait = ...
 
+class Browser:
+    def __init__(self, driver: WebDriver):
+        self.driver = driver
+        self.wait = WebDriverWait(
+            driver, timeout=2, ignored_exceptions=(WebDriverException,)
+        )
 
-def assert_(condition):
-    return wait.until(condition)
+    def assert_(self, condition):
+        return self.wait.until(condition)
 
+    def open(self, url):
+        self.driver.get(url)
 
-def open(url):
-    driver.get(url)
+    def back(self):
+        self.driver.back()
 
+    def quit(self):
+        self.driver.quit()
 
-def back():
-    driver.back()
+    def element(self, selector):
+        def find_visible(driver: WebDriver):
+            webelement = driver.find_element(*to_locator(selector))
+            if not webelement.is_displayed():
+                raise AssertionError(
+                    f'element is not displayed: {webelement.get_attribute("outerHTML")}'
+                )
+            return webelement
 
+        return self.wait.until(
+            find_visible, message=f'element by {selector} is not ready'
+        )
 
-def quit():
-    driver.quit()
+    def type(self, selector, value):
+        def command(driver: WebDriver) -> WebElement:
+            webelement = driver.find_element(*to_locator(selector))
+            webelement.send_keys(value)
+            return webelement
 
+        return self.wait.until(
+            command, message=f'failed to type «{value}» into element by {selector}'
+        )
 
-def element(selector):
-    def find_visible(driver: WebDriver):
-        webelement = driver.find_element(*to_locator(selector))
-        if not webelement.is_displayed():
-            raise AssertionError(f'element is not displayed: {webelement.get_attribute("outerHTML")}')
-        return webelement
+    def click(self, selector):
+        def command(driver: WebDriver) -> WebElement:
+            webelement = driver.find_element(*to_locator(selector))
+            webelement.click()
+            return webelement
 
-    return wait.until(find_visible, message=f'element by {selector} is not ready')
-
-
-def type(selector, value):
-    def command(driver: WebDriver) -> WebElement:
-        webelement = driver.find_element(*to_locator(selector))
-        webelement.send_keys(value)
-        return webelement
-
-    return wait.until(command, message=f'failed to type «{value}» into element by {selector}')
-
-
-def click(selector):
-    def command(driver: WebDriver) -> WebElement:
-        webelement = driver.find_element(*to_locator(selector))
-        webelement.click()
-        return webelement
-
-    return wait.until(command, message=f'failed to click on element by {selector}')
+        return self.wait.until(
+            command, message=f'failed to click on element by {selector}'
+        )
--- tests/test_ecosia.py
+++ tests/test_ecosia.py
@@ -1,45 +1,22 @@
 from selenium import webdriver
-from selenium.common import WebDriverException
 from selenium.webdriver import Keys
 from selenium.webdriver.chrome.service import Service as ChromeService
 
-from seleyasha.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 
-from seleyasha import browser
+from seleyasha.browser import Browser
 from seleyasha.conditions import that
 
-browser.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-browser.wait = WebDriverWait(browser.driver, timeout=2, ignored_exceptions=(WebDriverException,))
-
-# 3
-# browser = Browser(driver)
+browser = Browser(
+    webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+)
 
 browser.open('https://ecosia.org')
-# 2
-# browser.open('https://ecosia.org')
-
-'''
-# in Selene:
-browser.element('[name=q]').type('selene').press_enter()
-# in Selenium WebDriver:
-driver.find_element(By.CSS_SELECTOR, '[name=q]').send_keys('selene', Keys.ENTER)
-# OR with wait
-def find_element(driver):
-    return driver.find_element(By.CSS_SELECTOR, '[name=q]')
-wait.until(find_element).send_keys('selene', Keys.ENTER)
-# OR with built-in expected condition
-wait.until(visibility_of_element_located((By.NAME, 'q'))).send_keys('selene yashaka', Keys.ENTER)
-# OR with custom expected condition
-wait.until(element('[name=q]')).send_keys('selene yashaka', Keys.ENTER)
-'''
 
 query = '[name=q]'
 browser.type(query, value='selene' + Keys.ENTER)
-# 2
-# browser.type('[name=q]', value='selene' + Keys.ENTER)
-# 3
-# element('[name=q]').type('selene' + Keys.ENTER)
+# 4
+# browser.element('[name=q]').type('selene' + Keys.ENTER)
 # ...
 # query = element('[name=q]')
 # query.type('selene' + Keys.ENTER)
@@ -47,22 +24,11 @@ browser.type(query, value='selene' + Keys.ENTER)
 browser.back()
 
 browser.type(query, value=' github issues' + Keys.ENTER)
-# 2
-# browser.type(query, ' yashaka' + Keys.ENTER)
-# 3
+# 4
 # query.type(' yashaka' + Keys.ENTER)
 
-browser.click('[data-test-id=mainline-result-web-ABRA]:nth-of-type(1) a')
-# 2
-# browser.click('[data-test-id=mainline-result-web]:nth-of-type(1) a')
+browser.click('[data-test-id=mainline-result-web]:nth-of-type(1) a')
 
 browser.assert_(that.number_of_elements('[id^=issue_]:not([id$=_link])', value=25))
-# 2
-# browser.assert_that(number_of_elements('[id^=issue_]:not([id$=_link])', value=4))
-'''
-# less stable version:
-number_of_pulls = len(driver.find_elements(By.CSS_SELECTOR, '[id^=issue_]:not([id$=_link])'))
-assert number_of_pulls == 4
-'''
 
 browser.quit()
